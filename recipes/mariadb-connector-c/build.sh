f24f1cef94d922d0956ac4c40ec48ce01275b309
dcunn <daniel.cunningham@maxar.com>
2021-09-17 10:25:38 -0400

    new build.sh

--- recipes/mariadb-connector-c/build.sh
+++ recipes/mariadb-connector-c/build.sh
@@ -1,22 +1,18 @@
 #!/usr/bin/env bash
+set -e -x
 
-set -ex
-
-if [[ $target_platform =~ linux.* ]]; then
-  export LDFLAGS="$LDFLAGS -Wl,-rpath-link,$PREFIX/lib"
-fi
-
-mkdir build
-cd build
-
-# Set INSTALL_DOCREADMEDIR to a junk path to avoid installing the README into PREFIX
-cmake \
-    -DBUILD_SHARED_LIBS=ON \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+mkdir -p build && cd build
+cmake -GNinja \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_SKIP_INSTALL_ALL_DEPENDENCY=ON \
-    ..
+    -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+    -DWITH_MYSQLCOMPAT=1 \
+    -DINSTALL_LIBDIR=lib \
+    ../
 
-make -k -j${CPU_COUNT} || true
+ninja install
 
-make install
+#make -k -j${CPU_COUNT} || true
+
+#make install
+
+#make test
