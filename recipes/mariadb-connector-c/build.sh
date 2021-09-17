656f0af0721f423424404f1131602d7477fa3348
dcunn <daniel.cunningham@maxar.com>
2021-09-17 00:17:07 -0400

    edit build.sh

--- recipes/mariadb-connector-c/build.sh
+++ recipes/mariadb-connector-c/build.sh
@@ -2,6 +2,14 @@
 
 set -ex
 
+if [[ $target_platform =~ linux.* ]]; then
+  export LDFLAGS="$LDFLAGS -Wl,-rpath-link,$PREFIX/lib"
+fi
+
+mkdir build
+cd build
+
+# Set INSTALL_DOCREADMEDIR to a junk path to avoid installing the README into PREFIX
 cmake \
     -DBUILD_SHARED_LIBS=ON \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
@@ -12,5 +20,3 @@ cmake \
 make -k -j${CPU_COUNT} || true
 
 make install
-
-make test
